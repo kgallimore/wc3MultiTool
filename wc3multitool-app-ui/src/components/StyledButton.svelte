b8290588f1da5844952943fc96dcc0cdf93dcfdc
kgallimore <72752967+kgallimore@users.noreply.github.com>
2022-11-13 23:35:16 -0500

    UI pages

--- wc3multitool-app-ui/src/components/StyledButton.svelte
+++ wc3multitool-app-ui/src/components/StyledButton.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let label: string;
+  export let disabled: boolean = false;
+  export let active: boolean = false;
 </script>
 
-<div class="generic-btn content-center">
+<div class="generic-btn generic-btn-blue{active ? '-active' : ''} content-center">
   <div class="w-full h-full">
-    <button on:click type="button" class="cursor-pointer coture w-full h-full">
+    <button on:click type="button" class="coture w-full h-full" {disabled}>
       <strong>{label}</strong>
     </button>
   </div>
